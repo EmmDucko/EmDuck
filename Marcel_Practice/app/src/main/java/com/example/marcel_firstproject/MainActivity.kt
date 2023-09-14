287b653019dc19526521a4a4d53f6acd0c237564
VeryAwesomeSheep <baranek.marcel24@gmail.com>
2023-09-14 18:17:20 +0200

    [P] Marcel's first project done

--- Marcel_Practice/app/src/main/java/com/example/marcel_firstproject/MainActivity.kt
+++ Marcel_Practice/app/src/main/java/com/example/marcel_firstproject/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.marcel_firstproject
 
 import android.os.Bundle
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.padding
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.marcel_firstproject.ui.theme.Marcel_FirstProjectTheme
 
@@ -19,7 +23,7 @@ class MainActivity : ComponentActivity() {
             Marcel_FirstProjectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Greeting("Marcel")
                 }
             }
         }
@@ -28,10 +32,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+    Surface(color = Color.Red) {
+        Text(
+            text = "Hi, my name is $name!",
+            modifier = modifier.padding(24.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
